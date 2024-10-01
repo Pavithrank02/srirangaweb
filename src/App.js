dccf22b80cb722e5d6687ecf0b21ce9ee4c1c6db
Pavithran2 <pavithrank02@gmail.com>
2024-10-01 14:18:17 +0530

    image slider added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { ImagesSliderDemo } from "./components/ImageSlider";
 import { HeroParallaxDemo } from "./components/Show";
+
 
 function App() {
   return (
     <div className="App">
       <HeroParallaxDemo />
+      <ImagesSliderDemo />
     </div>
   );
 }
