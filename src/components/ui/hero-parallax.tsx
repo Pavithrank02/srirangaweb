cdae61b1bdfb2791fa47b991496ca1b8aea9939b
Pavithran2 <pavithrank02@gmail.com>
2024-10-11 11:35:44 +0530

    home page responsive added

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -2,11 +2,8 @@
 import React from "react";
 import { motion, useScroll, useTransform, useSpring } from "framer-motion";
 import { Link } from "react-router-dom";
-import { HoverBorderGradientDemo } from "../Button";
 
-export const HeroParallax = ({
-  products
-}) => {
+export const HeroParallax = ({ products }) => {
   const firstRow = products.slice(0, 5);
   const secondRow = products.slice(5, 10);
   const thirdRow = products.slice(10, 15);
@@ -18,16 +15,17 @@ export const HeroParallax = ({
 
   const springConfig = { stiffness: 300, damping: 30, bounce: 100 };
 
-  const translateX = useSpring(useTransform(scrollYProgress, [0, 1], [0, 1000]), springConfig);
-  const translateXReverse = useSpring(useTransform(scrollYProgress, [0, 1], [0, -1000]), springConfig);
+  const translateX = useSpring(useTransform(scrollYProgress, [0, 1], [0, 300]), springConfig);
+  const translateXReverse = useSpring(useTransform(scrollYProgress, [0, 1], [0, -800]), springConfig);
   const rotateX = useSpring(useTransform(scrollYProgress, [0, 0.2], [15, 0]), springConfig);
   const opacity = useSpring(useTransform(scrollYProgress, [0, 0.2], [0.2, 1]), springConfig);
   const rotateZ = useSpring(useTransform(scrollYProgress, [0, 0.2], [20, 0]), springConfig);
   const translateY = useSpring(useTransform(scrollYProgress, [0, 0.2], [-700, 500]), springConfig);
+
   return (
-    (<div
+    <div
       ref={ref}
-      className="h-[300vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]">
+      className="h-[300vh] xs:h-[200] py-20 md:py-40 overflow-hidden antialiased relative flex flex-col self-auto [perspective:800px] [transform-style:preserve-3d]">
       <Header />
       <motion.div
         style={{
@@ -37,48 +35,41 @@ export const HeroParallax = ({
           opacity,
         }}
         className="">
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-20">
+        {/* First Row */}
+        <motion.div className="flex flex-col items-center space-y-8 mb-10 md:flex-row-reverse md:space-y-0 md:space-x-reverse md:space-x-10">
           {firstRow.map((product) => (
             <ProductCard product={product} translate={translateX} key={product.title} />
           ))}
         </motion.div>
-        <motion.div className="flex flex-row  mb-20 space-x-20 ">
+        {/* Second Row */}
+        <motion.div className="flex flex-col items-center space-y-8 mb-10 md:flex-row md:space-y-0 md:space-x-20">
           {secondRow.map((product) => (
             <ProductCard product={product} translate={translateXReverse} key={product.title} />
           ))}
         </motion.div>
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20">
-          {thirdRow.map((product) => (
-            <ProductCard product={product} translate={translateX} key={product.title} />
-          ))}
-        </motion.div>
+        {/* Third Row */}
       </motion.div>
-    </div>)
+    </div>
   );
 };
 
 export const Header = () => {
   return (
-    (<div
-      className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
-      <h1 className="text-2xl md:text-7xl font-bold dark:text-black">
+    <div className="max-w-7xl relative mx-auto py-10 md:py-40 px-4 w-full left-0 top-16 xs:top-">
+      <h1 className="text-3xl md:text-7xl font-bold  dark:text-black">
         The Ultimate <br /> <span className="text-yellow-600">Sheet Metalling</span> Works
       </h1>
-      <p className="max-w-2xl text-base md:text-xl mt-8 text-black font-bold">
+      <p className="max-w-2xl text-base md:text-xl mt-8  text-black font-bold">
         We specialize in bending, welding, and finishing to meet your exact specifications
         with durability and precision.
       </p>
-
-    </div>)
+    </div>
   );
 };
 
-export const ProductCard = ({
-  product,
-  translate
-}) => {
+export const ProductCard = ({ product, translate }) => {
   return (
-    (<motion.div
+    <motion.div
       style={{
         x: translate,
       }}
@@ -86,8 +77,8 @@ export const ProductCard = ({
         y: -20,
       }}
       key={product.title}
-      className="group/product h-96 w-[30rem] relative flex-shrink-0">
-      <Link to={product.link} className="block group-hover/product:shadow-2xl ">
+      className="group/product h-64 w-full md:h-96  sm:w-[30rem] relative flex-shrink-0 mx-2 md:mx-0">
+      <Link to={product.link} className="block group-hover/product:shadow-2xl">
         <img
           src={product.thumbnail}
           height="600"
@@ -95,13 +86,11 @@ export const ProductCard = ({
           className="object-cover object-left-top absolute h-full w-full inset-0"
           alt={product.title} />
       </Link>
-      <div
-        className="absolute flex justify-center items-center inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none">
-        <h2
-          className="absolute  opacity-0 group-hover/product:opacity-100 text-white text-4xl font-bold">
+      <div className="absolute flex justify-center items-center inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none">
+        <h2 className="absolute opacity-0 group-hover/product:opacity-100 text-white text-lg md:text-4xl font-bold text-center">
           {product.title}
         </h2>
       </div>
-    </motion.div>)
+    </motion.div>
   );
 };
