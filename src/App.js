1d3d63991b8c30669b94f7ec0e85fc4b7acb9872
Pavithran2 <pavithrank02@gmail.com>
2024-10-15 11:26:31 +0530

    UI updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
-import Home from "./components/view/Home";
+import Home from "./view/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AboutUs from "./view/AboutUs";
@@ -21,7 +21,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/services" element={<Service />} />
+        <Route path="/product" element={<Service />} />
         <Route path="/capability" element={<Capabilities />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/laserservice" element={<LaserCutting />} />
