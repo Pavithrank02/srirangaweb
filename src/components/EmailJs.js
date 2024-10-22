1a9b90d96554cb8461e90d555da334bbf1edf426
Pavithran2 <pavithrank02@gmail.com>
2024-10-22 11:12:03 +0530

    contact page UI changed

--- src/components/EmailJs.js
+++ src/components/EmailJs.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
+import { FloatingDockDemo } from './FooterIcons';
 
 const ContactForm = () => {
   const form = useRef();
@@ -25,23 +26,58 @@ const ContactForm = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail} className="max-w-md mx-auto p-4 bg-gray-100 shadow-md">
-      <div className="mb-4">
-        <label className="block text-sm font-bold mb-2">Name</label>
-        <input type="text" name="user_name" className="w-full p-2 border" required />
+    <div className='p-10 flex flex-col justify-between h-full'>
+      <div className='flex flex-col sm:flex-row justify-evenly items-center '>
+
+        <div className="flex flex-col justify-between items-start md:items-start  ">
+          <div className="mb-6 text-black ">
+            <p className="font-bold text-sm md:text-xl mb-2">Contact Us:</p>
+            <div className=" font-semibold text-sm md:text-lg flex flex-row items-center mb-2">
+              <span className="d"> +91 97904 84558</span>
+            </div>
+            <div className=" font-semibold text-sm md:text-lg flex flex-row items-center mb-2">
+
+              <span className=""> srirangalaser.coimbatore@gmail.com</span>
+            </div>
+            <div className="font-semibold text-sm md:text-lg flex flex-row items-center">
+
+              <span className=""> 9/1E, Gandhi Nagar, Ondiputhur Road, Irugur, Coimbatore - 641103</span>
+
+            </div>
+
+          </div>
+          {/* Open Hours */}
+          <div className="mb-6">
+            <p className=" font-bold text-sm md:text-lg mb-2">Open Hours:</p>
+            <p className=" font-semibold text-sm md:text-base">
+              Monday-Friday: 7:00AM – 5:00PM<br />
+              Sat-Sun: CLOSED
+            </p>
+          </div>
+        </div>
+        <div className='w-3/5'>
+
+          <form ref={form} onSubmit={sendEmail} className="max-w-md mx-auto p-4 bg-yellow-600 shadow-md">
+
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2">Name</label>
+              <input type="text" name="user_name" className="w-full p-2 border" required />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2">Email</label>
+              <input type="email" name="user_email" className="w-full p-2 border" required />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2">Message</label>
+              <textarea name="message" rows="4" className="w-full p-2 border" required></textarea>
+            </div>
+            <button type="submit" className="bg-yellow-800 text-white px-4 py-2 rounded-md">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
-      <div className="mb-4">
-        <label className="block text-sm font-bold mb-2">Email</label>
-        <input type="email" name="user_email" className="w-full p-2 border" required />
-      </div>
-      <div className="mb-4">
-        <label className="block text-sm font-bold mb-2">Message</label>
-        <textarea name="message" rows="4" className="w-full p-2 border" required></textarea>
-      </div>
-      <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">
-        Send
-      </button>
-    </form>
+    </div>
   );
 };
 
--- src/view/Contact.js
+++ src/view/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ContactForm from '../components/EmailJs'
+import { ImagesSliderDemo } from '../components/Corousal'
 
 const Contact = () => {
   return (
-    <div className='mt-44'>
+    <div className=''>
+      <ImagesSliderDemo name={'Contact'} />
       <ContactForm />
     </div>
   )
